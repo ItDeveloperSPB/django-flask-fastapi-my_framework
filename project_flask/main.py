fcf28ae9839afebc68d0370d6058347aa649ab04
Nikolos123 <Isponec_2>
2023-02-01 17:18:50 +0300

    project my_project

--- project_flask/main.py
+++ project_flask/main.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# async def template_():
+#     return render_template('template.html',
+#                            title="Основной сайт",
+#                            object_list=[1, 23, 4, 5, 6])
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
+
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-async def template_():
-    return render_template('template.html',
-                           title="Основной сайт",
-                           object_list=[1, 23, 4, 5, 6])
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
